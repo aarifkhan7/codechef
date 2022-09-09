18915e715ceafa2f83a64dc8de265334a07c346d
aarif-khan <ajajchauhan@gmail.com>
2022-09-10 01:05:55 +0530

    September Long 1

---
+++ JUNE222D/Make_all_equal_using_Pairs.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+// Aarif Khan
+// Codechef: https://www.codechef.com/users/aarifkhan_7
+// Github: https://github.com/aarifkhan7
+
+#define pi (3.141592653589)
+#define MOD 1e9+7
+#define ll long long int
+#define all(x) x.begin(), x.end()
+#define range(a, b) for(int i = a; i < b; i++)
+#define range1(a, b) for(int j = a; j < b; j++)
+#define inint(x) int x; cin >> x;
+#define inarr(x, n) int arr[n]; range(0, n){cin >> x[i];}
+#define inll(x) ll x; cin >> x;
+#define instr(x) string x; cin >> x; 
+#define printc(cont) for(auto x : cont) cout << x << ' '; cout << '\n';
+#define printp(cont) for(auto x : cont) cout << x.first << ' ' << x.second << '\n';
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+#define pii pair<int,int>
+#define vi vector<int>
+#define pb push_back
+#define mp make_pair
+
+using namespace std;
+
+void solve(){
+    inint(n);
+    inarr(arr, n);
+    int count[1001];
+    range(0,1001) count[i] = 0;
+    range(0,n){
+        int tmp = arr[i];
+        count[tmp]++;
+    }
+    int max = *max_element(count, count + 1001);
+    cout << n - max << '\n';
+}
+
+int main()
+{
+    fast;
+    int t; cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
